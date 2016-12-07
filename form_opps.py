8056c5bf6a3d685ec2faf97a6639bb80549a4561
Chris Thurber <cthurber@me.com>
2016-12-06 22:00:52 -0500

    Consolidated form operations

---
+++ form_opps.py
@@ -0,0 +1,21 @@
+# from flask_wtf import Form
+from wtforms import Form, TextField, IntegerField, TextAreaField, SubmitField, RadioField, SelectField, SelectMultipleField, BooleanField, StringField, validators, ValidationError
+
+class Add_Asset_Form(Form):
+
+    city = TextField("city", [validators.DataRequired()])
+    contact = TextField("contact", [validators.DataRequired()])
+    descript = TextField("descript", [validators.DataRequired()])
+    idcode = TextField("id", [validators.DataRequired()])
+    lat = TextField("lat")
+    lon = TextField("lon")
+    name = TextField("name", [validators.DataRequired()])
+    state = TextField("state", [validators.DataRequired()])
+    street = TextField("street", [validators.DataRequired()])
+    telnum = TextField("telnum", [validators.DataRequired()])
+    website = TextField("website")
+    zipcode = TextField("zip", [validators.DataRequired()])
+
+class Delete_Asset_Form(Form):
+
+    idcode = TextField("id", [validators.DataRequired()])
